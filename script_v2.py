56eac432df6c9d8ba7e48727d7b5facbea08e45b
Quintiliano Blumenschein Andrade <quintiliano@solutinet.com.br>
2016-04-09 13:40:17 -0300

    Correções no script_v2.py

--- script_v2.py
+++ script_v2.py
@@ -2,6 +2,7 @@
 
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import os
+import sys
 import time
 import argparse
 from datetime import datetime
@@ -16,6 +17,7 @@ def converter(arquivo_original, max_pag_arquivo, tamanho_max, pasta_destino,
     cache_max_pag = max_pag_arquivo
     ultima_pagina = 0
     pagina_atual = 0
+    arquivos_criados = 0
 
     while(num_pag_total > 0):
         output = PdfFileWriter()
@@ -23,20 +25,22 @@ def converter(arquivo_original, max_pag_arquivo, tamanho_max, pasta_destino,
         nome_arquivo = pasta_destino + "/" + prefixo + str(pagina_atual) + ".pdf"
 
         for i in range(max_pag_arquivo):
-            print "Pagina atual ---", pagina_atual
             if pagina_atual == cache_pag_total:
                 break
-            output.addPage(inputpdf.getPage(pagina_atual))
+            else:
+                output.addPage(inputpdf.getPage(pagina_atual))
+
             pagina_atual = pagina_atual + 1
             contador = i
 
         print "Gravando arquivo ---", nome_arquivo
         outputStream = file(nome_arquivo, "wb")
         output.write(outputStream)
+        arquivos_criados += 1
 
         tamanho_arquivo_atual = os.stat(nome_arquivo).st_size
 
-        if tamanho_arquivo_atual >= tamanho_max:
+        if int(tamanho_arquivo_atual) >= int(tamanho_max):
             print "Arquivo muito grande, reduzindo quantidade de paginas."
             ultima_pagina = pagina_atual - max_pag_arquivo
             pagina_atual = ultima_pagina
@@ -51,9 +55,7 @@ def converter(arquivo_original, max_pag_arquivo, tamanho_max, pasta_destino,
             pagina_atual = ultima_pagina + 1
             num_pag_total = num_pag_total - (contador + 1)
 
-        print "Ultima pagina --", ultima_pagina
-        print "Restante --", num_pag_total
-        print "####"
+    print "Total de arquivos criados --", arquivos_criados
 
 
 def main():
@@ -90,6 +92,8 @@ def main():
 
     if prefixo is None:
         prefixo = datetime.now().strftime('%Y%m%d_')
+
+    tamanho_max = int(tamanho_max * 1024)
 
     converter(arquivo_original, max_pag_arquivo, tamanho_max, pasta_destino, prefixo)
 
